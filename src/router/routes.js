ce3fd27f240420609c17dab5cf6072a4f4205865
liuxiang7 <liuxiang7@tal.com>
2022-04-04 11:49:20 +0800

    11.2-router-link-命名路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,12 @@ import Blog from "@/views/Blog";
 import Project from "@/views/Project";
 import Message from "@/views/Message";
 
+// 命名路由:使用命名路由可以解除系统与路径之间的耦合
+// 比如下面的Blog组件，对应的path是/article
 export default [
   { name: "Home", path: "/", component: Home },
   { name: "About", path: "/about", component: About },
-  { name: "Blog", path: "/blog", component: Blog },
+  { name: "Blog", path: "/article", component: Blog },
   { name: "Project", path: "/project", component: Project },
   { name: "Message", path: "/message", component: Message },
 ];
