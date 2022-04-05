8b61b8fd5a400f8f2ec4f5eab8dc235e28678296
liuxiang7 <liuxiang7@tal.com>
2022-04-05 16:51:15 +0800

    17-首页-鼠标移动动效

--- src/views/Home/Carouselitem.vue
+++ src/views/Home/Carouselitem.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="carousel-item-container">
-    <div class="carousel-img">
+  <div
+    class="carousel-item-container"
+    ref="container"
+    @mousemove="handleMouseMove"
+    @mouseleave="handleMouseLeave"
+  >
+    <div class="carousel-img" ref="image" :style="imagePosition">
       <ImageLoader
         @load="this.showWords"
         :src="carousel.bigImg"
@@ -23,11 +28,43 @@ export default {
     return {
       titleWidth: 0,
       descWidth: 0,
+      containerSize: null, // 外层容器的尺寸
+      innerSize: null, // 里层图片的尺寸
+      mouseX: 0, // 鼠标的横坐标
+      mouseY: 0, // 鼠标的纵坐标
     };
+  },
+  computed: {
+    //得到图片坐标
+    imagePosition () {
+      if (!this.innerSize || !this.containerSize) {
+        return;
+      }
+      const extraWidth = this.innerSize.width - this.containerSize.width; // 多出的宽度
+      const extraHeight = this.innerSize.height - this.containerSize.height; //多出的高度
+      const left = (-extraWidth / this.containerSize.width) * this.mouseX;
+      const top = (-extraHeight / this.containerSize.height) * this.mouseY;
+      return {
+        transform: `translate(${left}px, ${top}px)`,
+      };
+    },
+    center () {
+      return {
+        x: this.containerSize.width / 2,
+        y: this.containerSize.height / 2,
+      };
+    },
   },
   mounted () {
     this.titleWidth = this.$refs.title.clientWidth;
     this.descWidth = this.$refs.desc.clientWidth;
+    this.setSize();
+    this.mouseX = this.center.x;
+    this.mouseY = this.center.y;
+    window.addEventListener("resize", this.setSize);
+  },
+  destroyed () {
+    window.removeEventListener("resize", this.resize);
   },
   methods: {
     // 调用该方法，显示文字
@@ -47,6 +84,26 @@ export default {
       this.$refs.desc.style.transition = "2s 1s";
       this.$refs.desc.style.width = this.descWidth + "px";
     },
+    setSize () {
+      this.containerSize = {
+        width: this.$refs.container.clientWidth,
+        height: this.$refs.container.clientHeight,
+      };
+
+      this.innerSize = {
+        width: this.$refs.image.clientWidth,
+        height: this.$refs.image.clientHeight,
+      };
+    },
+    handleMouseMove (e) {
+      const rect = this.$refs.container.getBoundingClientRect();
+      this.mouseX = e.clientX - rect.left;
+      this.mouseY = e.clientY - rect.top;
+    },
+    handleMouseLeave () {
+      this.mouseX = this.center.x;
+      this.mouseY = this.center.y;
+    },
   },
 };
 </script>
@@ -59,18 +116,23 @@ export default {
   height: 100%;
   color: #fff;
   position: relative;
+  overflow: hidden;
 }
 .carousel-img {
-  width: 100%;
-  height: 100%;
+  width: 110%;
+  height: 110%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  transition: 0.3s;
 }
+
 .title,
 .desc {
   position: absolute;
   letter-spacing: 3px;
   left: 30px;
   color: #fff;
-  // 防止文字看不清楚，加入描边text-shadow
   text-shadow: 1px 0 0 rgba(0, 0, 0, 0.5), -1px 0 0 rgba(0, 0, 0, 0.5),
     0 1px 0 rgba(0, 0, 0, 0.5), 0 -1px 0 rgba(0, 0, 0, 0.5);
   white-space: nowrap;
