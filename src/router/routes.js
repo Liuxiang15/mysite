c04f8d8def3809ade158d69495e0ea29943b3b7f
liuxiang7 <liuxiang7@tal.com>
2022-04-06 08:02:48 +0800

    23-文章详情页-mock-router

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,6 +3,7 @@ import About from "@/views/About";
 import Blog from "@/views/Blog";
 import Project from "@/views/Project";
 import Message from "@/views/Message";
+import BlogDetail from "@/views/Blog/Detail"
 
 // 命名路由:使用命名路由可以解除系统与路径之间的耦合
 // 比如下面的Blog组件，对应的path是/article
@@ -12,6 +13,7 @@ export default [
   { name: "Blog", path: "/article", component: Blog },
   // 动态路由
   { name: "CategoryBlog", path: "/article/cate/:categoryId", component: Blog },
+  { name: "BlogDetail", path: "/article/:id", component: BlogDetail },
   { name: "Project", path: "/project", component: Project },
   { name: "Message", path: "/message", component: Message },
 ];
