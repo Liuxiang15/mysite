ac360d9827105c78fea10b258fb4fb99a28bee9d
liuxiang7 <liuxiang7@tal.com>
2022-04-09 16:50:42 +0800

    37关于我-iframe

--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <h1>关于我</h1>
+  <div class="about-container" v-loading="loading || !srcLoaded">
+    <iframe
+      v-if="src"
+      class="about-content"
+      :src="src"
+      frameborder="0"
+      @load="srcLoaded = true"
+    ></iframe>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+export default {
+  data () {
+    return {
+      srcLoaded: false,
+    };
+  },
+  computed: mapState("about", {
+    src: "data",
+    loading: "loading",
+  }),
+  created () {
+    this.$store.dispatch("about/fetchAbout");
+  },
+};
 </script>
 
-<style></style>
+<style scoped lang="less">
+.about-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+  // 为了loading显示正确
+}
+.about-content {
+  width: 100%;
+  height: 100%;
+  // iframe默认是行盒
+  display: block;
+  box-sizing: border-box;
+}
+</style>
