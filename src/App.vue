bc16356ac1030f36e98ee7ac8edaf4a610f695bc
liuxiang7 <liuxiang7@tal.com>
2022-04-09 22:49:08 +0800

    40.1-异步组件测试

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,56 @@
 <template>
   <div class="app-container">
-    <Layout>
+    <!-- <Layout>
       <template #left>
         <div class="aside">
           <SiteAside />
         </div>
       </template>
-      <template #default> <RouterView /> </template>
+      <template #default>
+        <RouterView />
+      </template>
     </Layout>
-    <ToTop />
+    <ToTop /> -->
+    <AsyncComponent />
   </div>
 </template>
+
+
 <script>
+// 写法1
+// import MyComp from './MyComp'
+// const AsyncComponent = () => {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(MyComp)
+//     }, 3000)
+//   })
+// }
+
+// 写法2
+// const AsyncComponent = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(async () => {
+//       const MyComp = await import("./MyComp");
+//       resolve(MyComp);
+//     }, 3000);
+//   });
+// };
+
+
 import Layout from "./components/Layout";
 import SiteAside from "./components/SiteAside";
 import ToTop from "./components/ToTop";
-
 export default {
   components: {
     Layout,
     SiteAside,
     ToTop,
+    AsyncComponent,
   },
-}
+};
 </script>
+
 <style lang="less" scoped>
 @import '~@/styles/mixin.less';
 .app-container {
